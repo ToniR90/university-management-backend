f2192e5d28663488f80629ffefa09c61bf26208c
ToniR90 <romy90.t@gmail.com>
2025-11-20 11:41:19 +0100

    update: ApplicationConfig update with comments

--- src/main/java/com/orientation/backend/shared/infraestructure/config/ApplicationConfig.java
+++ src/main/java/com/orientation/backend/shared/infraestructure/config/ApplicationConfig.java
@@ -2,6 +2,23 @@ package com.orientation.backend.shared.infraestructure.config;
 
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * General application configuration.
+ * Placeholder for future beans and configurations.
+ *
+ * This class will contain shared beans like:
+ * - ModelMapper
+ * - ObjectMapper customizations
+ * - Clock beans for testing
+ * - etc.
+ */
 @Configuration
 public class ApplicationConfig {
+
+    // Future beans will be added here as needed
+    // Example:
+    // @Bean
+    // public ModelMapper modelMapper() {
+    //     return new ModelMapper();
+    // }
 }
