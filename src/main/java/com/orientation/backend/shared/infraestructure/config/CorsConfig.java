b358a3d69ac07c18f99def212111b9f6d26c4f74
ToniR90 <romy90.t@gmail.com>
2025-11-20 11:06:26 +0100

    feat: CorsConfig implementation

--- src/main/java/com/orientation/backend/shared/infraestructure/config/CorsConfig.java
+++ src/main/java/com/orientation/backend/shared/infraestructure/config/CorsConfig.java
@@ -1,7 +1,23 @@
 package com.orientation.backend.shared.infraestructure.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // Allow CORS for all paths
+                        .allowedOrigins("*")    // Allow requests from any origin(just for development, restrict in production)
+                        .allowedMethods("*")    // Allow GET, POST, PUT, DELETE, etc.
+                        .allowedHeaders("*");   //Content-Type, Authorization, etc.
+            }
+        };
+    }
 }
