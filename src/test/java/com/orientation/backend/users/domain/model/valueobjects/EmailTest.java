469a61c913eef60a3bdd07d496fd2606943dd6ab
ToniR90 <romy90.t@gmail.com>
2025-12-18 09:33:20 +0100

    feat: EmailTests implemented

--- src/test/java/com/orientation/backend/users/domain/model/valueobjects/EmailTest.java
+++ src/test/java/com/orientation/backend/users/domain/model/valueobjects/EmailTest.java
@@ -1,40 +1,75 @@
 package com.orientation.backend.users.domain.model.valueobjects;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+// AAA (Arrange-Act-Assert) Pattern
 
 class EmailTest {
 
     @Test
-    void of() {
+    void shouldCreateValidEmail() {
+        Email email = Email.of("test@test.com");
+
+        assertNotNull(email);
+        assertEquals("test@test.com", email.getValue());
     }
 
     @Test
-    void ofNullable() {
+    void shouldNormalizeEmailToLowercase() {
+        Email email = Email.of("TEST@TEST.COM");
+
+        assertEquals("test@test.com", email.getValue());
     }
 
     @Test
-    void getValue() {
+    void shouldTrimWhitespace() {
+        Email email = Email.of("  test@test.com  ");
+
+        assertEquals("test@test.com", email.getValue());
     }
 
     @Test
-    void getDomain() {
+    void shouldThrowExceptionForNull() {
+        assertThrows(NullPointerException.class, () -> {
+            Email.of(null);
+        });
     }
 
     @Test
-    void isFromDomain() {
+    void shouldThrowExceptionForInvalidFormat() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Email.of("invalid-email");
+        });
     }
 
     @Test
-    void testEquals() {
+    void shouldThrowExceptionForEmailWithoutAt() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Email.of("testtest.com");
+        });
     }
 
     @Test
-    void testHashCode() {
+    void shouldThrowExceptionForEmailWithoutDomain() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Email.of("test@");
+        });
     }
 
     @Test
-    void testToString() {
+    void shouldBeEqualByValue() {
+        Email email1 = Email.of("test@test.com");
+        Email email2 = Email.of("test@test.com");
+
+        assertEquals(email1, email2);
+    }
+
+    @Test
+    void shouldHaveSameHashCodeForSameValue() {
+        Email email1 = Email.of("test@test.com");
+        Email email2 = Email.of("test@test.com");
+
+        assertEquals(email1.hashCode(), email2.hashCode());
     }
 }
