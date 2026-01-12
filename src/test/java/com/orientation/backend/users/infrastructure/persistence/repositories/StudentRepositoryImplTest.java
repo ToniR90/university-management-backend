b7c96a3c994aa664805e3c2a97f444e05302f8cf
ToniR90 <romy90.t@gmail.com>
2026-01-12 09:54:58 +0100

    feat: Anotattions & Helper method implementation on StudentRepositoryImplTest.java

--- src/test/java/com/orientation/backend/users/infrastructure/persistence/repositories/StudentRepositoryImplTest.java
+++ src/test/java/com/orientation/backend/users/infrastructure/persistence/repositories/StudentRepositoryImplTest.java
@@ -1,7 +1,50 @@
 package com.orientation.backend.users.infrastructure.persistence.repositories;
 
+import com.orientation.backend.users.domain.model.entities.Student;
+import com.orientation.backend.users.domain.model.enums.CurrentYear;
+import com.orientation.backend.users.domain.model.valueobjects.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(StudentRepositoryImpl.class)
 class StudentRepositoryImplTest {
+
+    @Container
+    static PostgreSQLContainer<?> postres = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @Autowired
+    private StudentRepositoryImpl studentRepository;
+
+    // ========== Helper Methods ==========
+
+    private Student createTestStudent(String dni, String email){
+        return Student.builder()
+                .dni(Dni.of(dni))
+                .fullName(FullName.of("Test_Name", "Test_First_Surname", "Test_Second_Surname"))
+                .email(Optional.of(Email.of(email)))
+                .phone(Optional.of(Phone.of("+34612345678")))
+                .degree("Informàtica")
+                .currentYear(CurrentYear.FIRST)
+                .alumniInfo(AlumniInfo.notAlumni())
+                .rgpdConsent(RgpdConsent.pending())
+                .build();
+    }
+
+    // ========== CRUD Tests ==========
 
 }
