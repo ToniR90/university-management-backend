120f3ac52aef6f869383f974d1309e03d3b79525
ToniR90 <romy90.t@gmail.com>
2026-01-15 12:48:05 +0100

    feat: souldCreateStudentSuccessfully() test implemented on StudentServiceTest.java

--- src/test/java/com/orientation/backend/users/application/services/StudentServiceTest.java
+++ src/test/java/com/orientation/backend/users/application/services/StudentServiceTest.java
@@ -1,15 +1,21 @@
 package com.orientation.backend.users.application.services;
 
+import com.orientation.backend.users.application.commands.CreateStudentCommand;
 import com.orientation.backend.users.domain.model.entities.Student;
 import com.orientation.backend.users.domain.model.enums.CurrentYear;
 import com.orientation.backend.users.domain.model.valueobjects.*;
 import com.orientation.backend.users.domain.repository.StudentRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
@@ -24,7 +30,7 @@ class StudentServiceTest {
     private Student createTestStudent(String dni, String email) {
         return Student.builder()
                 .dni(Dni.of(dni))
-                .fullName(FullName.of("Test_Name", "Test_Surname1", "Test_Surname2"))
+                .fullName(FullName.of("test_name", "test_firstSurname", "test_secondSurname"))
                 .email(Optional.of(Email.of(email)))
                 .phone(Optional.of(Phone.of("+34612345678")))
                 .degree("Videojocs")
@@ -32,6 +38,73 @@ class StudentServiceTest {
                 .alumniInfo(AlumniInfo.notAlumni())
                 .rgpdConsent(RgpdConsent.pending())
                 .build();
+    }
+
+    private CreateStudentCommand createTestStudentCommand(String dni, String email) {
+
+        return new CreateStudentCommand(
+                dni,
+                "test_name",
+                "test_firstSurname",
+                "test_secondSurname",
+                email,
+                "+34612345678",
+                "Videojocs",
+                "FIRST"
+        );
+    }
+
+    @Test
+    void shouldCreateStudentSuccessfully() {
+
+        // ====================
+        // ARRANGE
+        // ====================
+
+        CreateStudentCommand command = createTestStudentCommand("00000000T", "test@email.com");
+
+        Student expectedStudent = createTestStudent("00000000T", "test@email.com");
+
+        // Forced false with any Dni match
+        when(studentRepository.existsByDni(any(Dni.class))).thenReturn(false);
+
+        // Forced to return the expectedStudent
+        when(studentRepository.save(any(Student.class))).thenReturn(expectedStudent);
+
+
+        // ====================
+        // ACT
+        // ====================
+
+        Student result = studentService.createStudent(command);
+
+        // ====================
+        // ASSERT
+        // ====================
+
+        assertNotNull(result);
+        assertEquals("00000000T", result.getDni().getValue());
+        assertEquals("test_name", result.getFullName().getName());
+        assertEquals("test_firstSurname", result.getFullName().getFirstSurname());
+        assertEquals("test_secondSurname", result.getFullName().getSecondSurname());
+
+        assertTrue(result.getEmail().isPresent());
+        assertEquals("test@email.com", result.getEmail().get().getValue());
+
+        assertTrue(result.getPhone().isPresent());
+        assertEquals("+34612345678", result.getPhone().get().getValue());
+
+        assertEquals(CurrentYear.FIRST, result.getCurrentYear());
+
+        // ======================
+        // VERIFY
+        // ======================
+
+        verify(studentRepository, times(1))
+                .existsByDni(any(Dni.class));
+
+        verify(studentRepository, times(1))
+                .save(any(Student.class));
     }
 
 }
