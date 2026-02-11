80681e177a797a7dec0e15d5bd5e3deae0b5b044
ToniR90 <romy90.t@gmail.com>
2026-02-11 16:22:58 +0100

    feat: PATCH implemented

--- src/main/java/com/orientation/backend/users/infrastructure/web/controller/StudentController.java
+++ src/main/java/com/orientation/backend/users/infrastructure/web/controller/StudentController.java
@@ -1,9 +1,15 @@
 package com.orientation.backend.users.infrastructure.web.controller;
 
 import com.orientation.backend.users.application.commands.CreateStudentCommand;
+import com.orientation.backend.users.application.commands.MarkAsAlumniCommand;
+import com.orientation.backend.users.application.commands.UpdateContactCommand;
+import com.orientation.backend.users.application.commands.UpdateRgpdConsentCommand;
 import com.orientation.backend.users.application.services.StudentService;
 import com.orientation.backend.users.domain.model.entities.Student;
 import com.orientation.backend.users.infrastructure.web.dto.request.CreateStudentRequest;
+import com.orientation.backend.users.infrastructure.web.dto.request.MarkAlumniRequest;
+import com.orientation.backend.users.infrastructure.web.dto.request.UpdateContactRequest;
+import com.orientation.backend.users.infrastructure.web.dto.request.UpdateRgpdRequest;
 import com.orientation.backend.users.infrastructure.web.dto.response.StudentResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -59,5 +65,44 @@ public class StudentController {
         StudentResponse response = StudentResponse.fromDomain(student);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PatchMapping("/{id}/contact")
+    public ResponseEntity<StudentResponse> updateContact(@PathVariable Long id, @Valid @RequestBody UpdateContactRequest request){
+        UpdateContactCommand command = new UpdateContactCommand(
+                request.email(),
+                request.phone()
+        );
+
+        Student student = studentService.updateContactInfo(id, command);
+        StudentResponse response = StudentResponse.fromDomain(student);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{id}/rgpd")
+    public ResponseEntity<StudentResponse> updateRgpd(@PathVariable Long id, @Valid @RequestBody UpdateRgpdRequest request){
+        UpdateRgpdConsentCommand command = new UpdateRgpdConsentCommand(
+                request.rgpdConsentStatus(),
+                request.signedYear()
+        );
+
+        Student student = studentService.updateRgpdConsent(id, command);
+        StudentResponse response = StudentResponse.fromDomain(student);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{id}/alumni")
+    public ResponseEntity<StudentResponse> updateAlumni(@PathVariable Long id, @Valid @RequestBody MarkAlumniRequest request){
+        MarkAsAlumniCommand command = new MarkAsAlumniCommand(
+                request.alumniType(),
+                request.graduationYear()
+        );
+
+        Student student = studentService.markAsAlumni(id, command);
+        StudentResponse response = StudentResponse.fromDomain(student);
+
+        return ResponseEntity.ok(response);
     }
 }
