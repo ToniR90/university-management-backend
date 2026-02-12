9594347c8031b7608c1154c36dc3706d3bba38fd
ToniR90 <romy90.t@gmail.com>
2026-02-12 16:03:44 +0100

    feat: added deactivate() tests in StudentTest.java

--- src/test/java/com/orientation/backend/users/domain/model/entities/StudentTest.java
+++ src/test/java/com/orientation/backend/users/domain/model/entities/StudentTest.java
@@ -1,6 +1,7 @@
 package com.orientation.backend.users.domain.model.entities;
 
 import com.orientation.backend.users.domain.model.enums.*;
+import com.orientation.backend.users.domain.model.exceptions.StudentAlreadyInactiveException;
 import com.orientation.backend.users.domain.model.valueobjects.Dni;
 import com.orientation.backend.users.domain.model.valueobjects.Email;
 import com.orientation.backend.users.domain.model.valueobjects.FullName;
@@ -421,15 +422,32 @@ class StudentTest {
 
     @Test
     void shouldBeActiveWhenCreateStudent() {
+        // ARRANGE
+        Student student = createStudent();
+
+        // ACT + ASSERT
+        assertTrue(student.isActive());
+        assertTrue(student.getDeletedAt().isEmpty());
     }
 
     @Test
     void shouldNotBeActiveWhenIsDeactivated() {
+        // ARRANGE
+        Student student = createStudent();
 
+        // ACT + ASSERT
+        student.deactivate();
+        assertFalse(student.isActive());
+        assertTrue(student.getDeletedAt().isPresent());
     }
 
     @Test
     void shouldThrowExceptionIfAlreadyDeactivated() {
+        // ASSERT
+        Student student = createStudent();
+        student.deactivate();
 
+        // ACT + ASSERT
+        assertThrows(StudentAlreadyInactiveException.class, student::deactivate);
     }
 }
