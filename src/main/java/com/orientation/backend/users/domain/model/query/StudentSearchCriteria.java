48dd6c55d63569ff64342891f16d7629b11ccc08
ToniR90 <romy90.t@gmail.com>
2026-03-04 10:43:02 +0100

    feat: StudentSearchCriteria.java implementation as a filter class

--- src/main/java/com/orientation/backend/users/domain/model/query/StudentSearchCriteria.java
+++ src/main/java/com/orientation/backend/users/domain/model/query/StudentSearchCriteria.java
@@ -1,5 +1,39 @@
 package com.orientation.backend.users.domain.model.query;
 
+import com.orientation.backend.users.domain.model.enums.CurrentYear;
+
+/**
+ * This class encapsule all the parameters the client can send to make the filters
+ * All the parameters can be null, as the filters are optional
+ */
 public class StudentSearchCriteria {
 
+    private final String name;
+    private final String dni;
+    private final CurrentYear currentYear;
+    private final Boolean isAlumni;
+
+
+    public StudentSearchCriteria(String name, String dni, CurrentYear currentYear, Boolean isAlumni) {
+        this.name = name;
+        this.dni = dni;
+        this.currentYear = currentYear;
+        this.isAlumni = isAlumni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public CurrentYear getCurrentYear() {
+        return currentYear;
+    }
+
+    public Boolean getIsAlumni() {
+        return isAlumni;
+    }
 }
