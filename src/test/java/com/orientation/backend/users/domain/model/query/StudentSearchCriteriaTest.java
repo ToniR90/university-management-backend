1231c47ee9278b329ec25476ec286c41a96f33fe
ToniR90 <romy90.t@gmail.com>
2026-03-05 15:39:59 +0100

    feat: StudentSearchCriteriaTest.java implemented

---
+++ src/test/java/com/orientation/backend/users/domain/model/query/StudentSearchCriteriaTest.java
@@ -0,0 +1,35 @@
+package com.orientation.backend.users.domain.model.query;
+
+import com.orientation.backend.users.domain.model.enums.CurrentYear;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * AAA testing
+ */
+
+class StudentSearchCriteriaTest {
+
+    @Test
+    void shouldCreateValidStudentSearchCriteriaWithAllFields() {
+        StudentSearchCriteria criteria = new StudentSearchCriteria("Dani", "12345678Z", CurrentYear.FIRST, true);
+
+        assertNotNull(criteria);
+        assertEquals("Dani", criteria.getName());
+        assertEquals("12345678Z", criteria.getDni());
+        assertEquals(CurrentYear.FIRST, criteria.getCurrentYear());
+        assertTrue(criteria.getIsAlumni());
+    }
+
+    @Test
+    void shouldCreateValidStudentSearchCriteriaWithEmptyFields() {
+        StudentSearchCriteria criteria = new StudentSearchCriteria(null, null, null, null);
+
+        assertNotNull(criteria);
+        assertNull(criteria.getName());
+        assertNull(criteria.getDni());
+        assertNull(criteria.getCurrentYear());
+        assertNull(criteria.getIsAlumni());
+    }
+}
