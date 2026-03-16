8c5f80838bff6e234310e350838501292ede74d4
ToniR90 <romy90.t@gmail.com>
2026-03-16 11:47:26 +0100

    update: StudentSearchCriteria.java with degree enum

--- src/main/java/com/orientation/backend/users/domain/model/query/StudentSearchCriteria.java
+++ src/main/java/com/orientation/backend/users/domain/model/query/StudentSearchCriteria.java
@@ -1,6 +1,7 @@
 package com.orientation.backend.users.domain.model.query;
 
 import com.orientation.backend.users.domain.model.enums.CurrentYear;
+import com.orientation.backend.users.domain.model.enums.Degree;
 
 /**
  * This class encapsule all the parameters the client can send to make the filters
@@ -12,13 +13,15 @@ public class StudentSearchCriteria {
     private final String dni;
     private final CurrentYear currentYear;
     private final Boolean isAlumni;
+    private final Degree degree;
 
 
-    public StudentSearchCriteria(String name, String dni, CurrentYear currentYear, Boolean isAlumni) {
+    public StudentSearchCriteria(String name, String dni, CurrentYear currentYear, Boolean isAlumni, Degree degree) {
         this.name = name;
         this.dni = dni;
         this.currentYear = currentYear;
         this.isAlumni = isAlumni;
+        this.degree = degree;
     }
 
     public String getName() {
@@ -35,5 +38,9 @@ public class StudentSearchCriteria {
 
     public Boolean getIsAlumni() {
         return isAlumni;
+    }
+
+    public Degree getDegree() {
+        return degree;
     }
 }
