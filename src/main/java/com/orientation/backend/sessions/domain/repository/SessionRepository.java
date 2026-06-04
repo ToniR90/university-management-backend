82b78bd101cebb5c85050c5140d65ec666bd9ea4
ToniR90 <romy90.t@gmail.com>
2026-06-04 23:41:19 +0200

    update: SessionRepository.java with findCancellable method

--- src/main/java/com/orientation/backend/sessions/domain/repository/SessionRepository.java
+++ src/main/java/com/orientation/backend/sessions/domain/repository/SessionRepository.java
@@ -1,10 +1,12 @@
 package com.orientation.backend.sessions.domain.repository;
 
 import com.orientation.backend.sessions.domain.model.entities.Session;
+import com.orientation.backend.sessions.domain.model.query.SessionSearchCriteria;
 
 import java.util.List;
 
 public interface SessionRepository {
     Session save(Session session);
     List<Session> findAll();
+    List<Session> findCancellable(SessionSearchCriteria criteria);
 }
